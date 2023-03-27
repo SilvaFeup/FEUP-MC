131c436f9e4aa30dbda31aef35fc954c20770e58
Aurel <aurelien.dafonseca@gmail.com>
2023-03-27 17:23:15 +0100

    some correction on terminal layouts

--- TerminalApplication/app/src/main/java/com/uporto/terminalapplication/MainActivity.kt
+++ TerminalApplication/app/src/main/java/com/uporto/terminalapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.uporto.terminalapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.main_bt_checkout)
+
+        button.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0 : View?){
+                val resultIntent = Intent(this@MainActivity,result_activity::class.java)
+                startActivity(resultIntent)
+            }
+        })
     }
 }
--- TerminalApplication/app/src/main/java/com/uporto/terminalapplication/result_activity.kt
+++ TerminalApplication/app/src/main/java/com/uporto/terminalapplication/result_activity.kt
@@ -1,11 +1,26 @@
 package com.uporto.terminalapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class result_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val button = findViewById<Button>(R.id.result_bt_back)
+
+
+        //TODO set text of both messages
+
+        button.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val mainIntent = Intent(this@result_activity,MainActivity::class.java)
+                startActivity(mainIntent)
+            }
+        })
     }
 }
