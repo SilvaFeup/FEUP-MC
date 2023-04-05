f2be1ab4369970a665e1bae6b4c9fe3777b825ec
Aurel <aurelien.dafonseca@gmail.com>
2023-04-05 19:13:56 +0100

    the terminal run the scan app

--- TerminalApplication/app/src/main/java/com/uporto/terminalapplication/result_activity.kt
+++ TerminalApplication/app/src/main/java/com/uporto/terminalapplication/result_activity.kt
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class result_activity : AppCompatActivity() {
+    private val button by lazy { findViewById<Button>(R.id.result_bt_back)}
+    private val tv_result by lazy { findViewById<TextView>(R.id.result_tv_outcome) }
+    private val tv_gate by lazy { findViewById<TextView>(R.id.result_tv_doorMessage) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val button = findViewById<Button>(R.id.result_bt_back)
 
 
-        //TODO set text of both messages
+        //TODO set text of both messages with the list of products
+        tv_result.text = intent.getStringExtra("result")
+
 
         button.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
