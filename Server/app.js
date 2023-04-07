5b7dd0c54db3ad5eb5004214d0fd6d42829ba599
JoaoSilva <up201906478@up.pt>
2023-04-07 19:57:40 +0100

    Server corrected

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,9 @@ const koa = require('koa');
 
 const app = new koa();
 const router = require('./router');
+const bodyParser = require('koa-bodyparser');
+
+app.use(bodyParser());
 
 app.use(router.routes());
 app.listen(3000, () => console.log('Server started on port 3000'));
