7e0446b89941dfec69e221af1240afe55faaeeb4
Aurel <aurelien.dafonseca@gmail.com>
2023-04-09 18:15:18 +0100

    add products in basket with QR code

--- CustommerApplication/app/src/main/java/fr/kodo/myapplication/WelcomeActivity.kt
+++ CustommerApplication/app/src/main/java/fr/kodo/myapplication/WelcomeActivity.kt
@@ -1,14 +1,21 @@
 package fr.kodo.myapplication
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import fr.kodo.myapplication.controller.ShoppingBasketAdapter
 import fr.kodo.myapplication.model.Product
 
+private const val ACTION_SCAN = "com.google.zxing.client.android.SCAN"
 class WelcomeActivity : AppCompatActivity() {
 
     var shoppingBasket = ArrayList<Product>()
@@ -24,9 +31,55 @@ class WelcomeActivity : AppCompatActivity() {
         shoppingBasketView.adapter = ShoppingBasketAdapter(shoppingBasket)
         shoppingBasketView.layoutManager = LinearLayoutManager(this)
 
-        btAddProduct.setOnClickListener {
-            shoppingBasket.add(Product(shoppingBasket.size, "Product ${shoppingBasket.size}", 10.0, 1))
-            shoppingBasketView.adapter?.notifyItemInserted(shoppingBasket.size - 1)
+        btAddProduct.setOnClickListener { scan() }
+    }
+
+
+    private fun showDialog(act: Activity, title: CharSequence, message: CharSequence, buttonYes: CharSequence, buttonNo: CharSequence): AlertDialog {
+        val downloadDialog = AlertDialog.Builder(act)
+        downloadDialog.setTitle(title)
+        downloadDialog.setMessage(message)
+        downloadDialog.setPositiveButton(buttonYes) { _, _ ->
+            val uri = Uri.parse("market://search?q=pname:com.google.zxing.client.android")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            act.startActivity(intent)
+        }
+        downloadDialog.setNegativeButton(buttonNo, null)
+        return downloadDialog.create()
+    }
+
+    private fun scan(){
+        try {
+            val intent = Intent(ACTION_SCAN).apply {
+                putExtra("SCAN_MOD", "QR_CODE_MODE")}
+            startActivityForResult(intent,0)
+        }
+        catch (e: ActivityNotFoundException){
+            showDialog(this, "No Scanner Found", "Download a scanner code app?", "Yes", "No").show()
         }
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0){
+            if (resultCode == AppCompatActivity.RESULT_OK){
+                var product =data?.getStringExtra("SCAN_RESULT")?:""
+                if (product != "") {
+
+                    var result = product.split(",")
+
+                    shoppingBasket.add(
+                        Product(
+                            shoppingBasket.size,
+                            result[0].toString(),
+                            result[1].toDouble(),
+                            1
+                        )
+                    )
+                    shoppingBasketView.adapter?.notifyItemInserted(shoppingBasket.size - 1)
+                }
+            }
+        }
+    }
+
 }
+
--- CustommerApplication/app/src/main/java/fr/kodo/myapplication/controller/AuthController.kt
+++ CustommerApplication/app/src/main/java/fr/kodo/myapplication/controller/AuthController.kt
@@ -14,7 +14,7 @@ class AuthController {
 
     val apiInterface: APIInterface by lazy {
         Retrofit.Builder()
-            .baseUrl("http://192.168.1.81:3000/")
+            .baseUrl("http://192.168.1.80:3000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(APIInterface::class.java)
